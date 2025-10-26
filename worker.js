368aa61829fd42fd2898ee669dc9426b808159b0
awes956 <108942403+awes956@users.noreply.github.com>
2025-10-26 16:46:48 +0530

    Add worker.js for syntax highlighting using highlight.js

---
+++ worker.js
@@ -0,0 +1,1 @@
+importScripts("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js");onmessage=e=>{try{postMessage(hljs.highlightAuto(e.data).value)}catch(t){postMessage(e.data)}};
