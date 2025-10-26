5ff6dbfcdfacda842591aea4d92cb0433c23a55c
CollaborativeTextArea <sharedtextarea1100@gmail.com>
2025-10-26 17:12:38 +0530

    Delete worker.js

--- worker.js
+++
@@ -1,1 +0,0 @@
-importScripts("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js");onmessage=e=>{try{postMessage(hljs.highlightAuto(e.data).value)}catch(t){postMessage(e.data)}};
